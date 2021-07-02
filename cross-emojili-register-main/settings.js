dd764d883ed5647f8d404a02c6cdb4bfb7fe8519
Tokyo01 <86851671+Tokyo01@users.noreply.github.com>
2021-07-02 21:39:56 +0300

    Add files via upload

---
+++ cross-emojili-register-main/settings.js
@@ -0,0 +1,38 @@
+module.exports = {
+    bot: {
+        botOwner: ["813400017137827911"],
+        botToken: "ODYwNTgyODUxNTQ3NjI3NTcx.YN9WPA.VISjTF56H-eDUa3XQO9vWfXVztg",
+        botPrefix: ".",
+        botStatus: " TØKYØ ❤️ Public kayıt bot"
+
+
+    },
+    guild: {
+        id: "",
+        tag: "¹⁶⁶⁰",
+        unTag: "•"
+    },
+
+    roles: {
+        manRoles: [860583349688467456],
+        womanRoles: [860575671558471722],
+        unregisterRoles: [860584548416618518],
+        registerStaff: "860584412269903902",
+        Suspecious: "860584309496872980",
+        vipRole: "860575671558471726",
+        boosterRole: "860584067603234846",
+        botCommands: "860575672245026818",
+        tagRole: "860583905190871050"
+    },
+    channel: {
+        rules: "",
+        botVoice: "",
+        registerChat: ""
+    },
+    emojis: {
+        yes: "860583739184119858",
+        no: "860583794741477458",
+        man: "860583349688467456", // erkek ve kız emoji ıd
+        woman: "860575671558471722"
+    }
+};
